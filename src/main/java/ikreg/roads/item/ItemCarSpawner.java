d62d4dc9f0f14083527a81552b1478444843aa86
Gero G. <11deutron11@gmail.com>
2016-04-07 22:17:38 +0200

    Oh, eclipse does not automatically add Files for you. So here goes the Car Spawner class

---
+++ src/main/java/ikreg/roads/item/ItemCarSpawner.java
@@ -0,0 +1,34 @@
+package ikreg.roads.item;
+
+import ikreg.roads.entity.EntityTestCar;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ItemCarSpawner extends Item {
+
+	public ItemCarSpawner() {
+		GameRegistry.registerItem(this, getName());
+		setUnlocalizedName(getName());
+	}
+
+	public String getName() {
+		return "carSpawner";
+	}
+	
+	@Override
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		EntityTestCar newAnt = new EntityTestCar(worldIn);
+		newAnt.setLocationAndAngles(pos.getX() + 0.5d, pos.getY() + 1.1d, pos.getZ() + 0.5d, 0F, 0F);
+		worldIn.spawnEntityInWorld(newAnt);
+		return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+	}
+
+}
